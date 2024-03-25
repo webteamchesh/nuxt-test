e34fb704fbd415254362b0e02e50c77c2410279b
antiloquax <tranter.m@sky.com>
2024-03-25 18:34:48 +0000

    First commit

---
+++ app.vue
@@ -0,0 +1,17 @@
+
+<script setup lang="ts">
+const date = new Date().toLocaleString('en-GB')
+</script>
+
+<template>
+  <div>
+    <h1>Nuxt test</h1>
+    <p>{{ date }}</p>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  color: DarkBlue;
+}
+</style>
