8dfa57d7945eb3c2f7a9c2edec863063e0e1249b
antiloquax <tranter.m@sky.com>
2024-03-26 16:01:33 +0000

    Update app.vue

--- app.vue
+++ app.vue
@@ -1,12 +1,12 @@
 
 <script setup lang="ts">
-const date = new Date().toLocaleString('en-GB')
+
 </script>
 
 <template>
   <div class="container mt-3">
     <h1>Nuxt test</h1>
-    <p>{{ date }}</p>
+    
     <entry-listing></entry-listing>
   </div>
 </template>
