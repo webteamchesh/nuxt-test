81a2a6d7b6422f98cd98481ce66f67108f608058
antiloquax <tranter.m@sky.com>
2024-03-28 16:46:13 +0000

    Delete components directory

--- components/myFooter.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="row footer">
-    <p class="m-0 p-0 pt-1">Footer</p>
-  </div>
-</template>
--- components/myHeader.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="row header">
-    <p class="m-0 p-0 pt-1">Header</p>
-  </div>
-</template>
