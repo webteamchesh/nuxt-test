8c1b0299550515a1057dd05ea5252663586a33a3
antiloquax <tranter.m@sky.com>
2024-03-30 09:51:27 +0000

    Trying to use webpack instead

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,12 @@
 export default defineNuxtConfig({
+  builder: 'webpack',
   plugins: [
     '~/plugins/contensis'
   ],
   devtools: { enabled: true},
   css: ["bootstrap/dist/css/bootstrap.min.css"],
   app: {
-    buildAssetsDir: 'static',
+    buildAssetsDir: '/static/',
   },
   modules: [
     '@pinia/nuxt',
