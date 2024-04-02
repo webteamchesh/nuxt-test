afbeab0a2a0d95108c8559a70ac05cec46a2e7f8
antiloquax <tranter.m@sky.com>
2024-04-02 09:27:54 +0100

    Chore - missed file

--- app.vue
+++ app.vue
@@ -1,55 +1,62 @@
 <script setup lang="ts">
+// Optionally log all the env variables
+let keys = Object.keys(process.env);
+keys.sort();
+let myEnv = keys.reduce((acc,k) => {
+  return [...acc, `${k}: ${process.env[k]}`];
+}, []);
+console.log(myEnv);
 
-import { useAppStore } from '~/stores/app';
-const appStore = useAppStore();
-const { setItems, setH1, setContentType, setTitle, setDescription } = appStore;
+
+// Bring in the Contensis client from plugins.
 const { $client } = useNuxtApp();
 
-await callOnce(async () => {
-  let query = useRoute().query;
-  try {
+// Get the query string so we can identify the entry attached to this node.
+const query = useRoute().query;
+
+// Set up some state.
+const title  = useState('title', "Page not found");
+const description = useState('description', "");
+const items = useState('items', "[]");
+let contentType;
+
+// Use the Contensis client to fetch the entry and set some state.
+if (query.entryId) {
+  await callOnce(async () => {
     let { data } = await useAsyncData('data', () =>
       $client.entries.get(query.entryId),
     );
     if (data.value) {
-      setTitle(data.value.title);
-      setH1(data.value.h1);
-      setDescription(data.value.description);
-      setContentType(data.value.contentTypeId);
+      title.value = data.value.entryTitle;
+      description.value = data.value.entryDescription;
+      contentType = data.value.contentTypeId;
     }
-  } catch (error) {
-    console.log(error);
-  }
-});
+  });
+}
 
-await callOnce(async () => {
-  try {
+// This app is intended to use for a listing page.
+// So here we fetch the entries that we need to list.
+if (contentType) {
+  await callOnce(async () => {
     let { data } = await useAsyncData('data', () =>
-      $client.entries.list({
-        contentTypeId: appStore.getContentType,
-        versionStatus: 'latest',
-        pageOptions: {
-          pageSize: 100,
-        },
-      }),
+      $client.entries.list(contentType),
     );
     if (data.value) {
-      setItems(data.value.items);
+      items.value = data.value.items;
     }
-  } catch (error) {
-    console.log(error);
-  }
-});
+  });
+}
 
+// Set the favicon path.
 useHead({
   link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
 });
-
 </script>
+
 <template>
   <div class="container-fluid p-0">
     <MyHeader />
-    <NuxtPage/>
+    <NuxtPage />
     <MyFooter />
   </div>
 </template>
