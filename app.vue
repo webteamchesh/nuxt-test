0c24fa342000d5a7e16dcc01a63a0dd7317c76aa
antiloquax <tranter.m@sky.com>
2024-04-04 09:26:58 +0100

    Add files via upload

--- app.vue
+++ app.vue
@@ -7,7 +7,7 @@ const { $client } = useNuxtApp();
 const query = useRoute().query;
 
 // Set up some state.
-const title  = useState('title', () => "");
+const title  = useState('title', () => "Page not found");
 const description = useState('description', () => "");
 const items = useState('items', () => []);
 //const path = useState('path', "")
@@ -15,46 +15,40 @@ let contentType;
 
 // Get path from node.
 //if (query.nodeId) {
-//  await callOnce(async () => {
 //    let { data } = await useAsyncData('data', () =>
 //      $client.nodes.get(query.nodeId),
 //    );
 //    if (data.value) {
 //      path.value = `${data.value.path}/`;
 //    }
-//  });
 //}
 
 // Use the Contensis client to fetch the entry and set some state.
 if (query.entryId) {
-  await callOnce(async () => {
-    let { data } = await useAsyncData('data', () =>
-      $client.entries.get(query.entryId),
-    );
-    if (data.value) {
-      title.value = data.value.entryTitle;
-      description.value = data.value.entryDescription;
-      contentType = data.value.contentTypeId;
-    }
-  });
-}
+  let { data, error } = await useAsyncData('data', () =>
+    $client.entries.get(query.entryId),
+  );
+  if (!error.value) {
+    title.value = data.value.entryTitle;
+    description.value = data.value.entryDescription;
+    contentType = data.value.contentTypeId;
+  }
+} 
 
 // This app is intended to use for a listing page.
 // So here we fetch the entries that we need to list.
 if (contentType) {
-  await callOnce(async () => {
-    let { data } = await useAsyncData('data', () =>
+    let { data, error } = await useAsyncData('data', () =>
       $client.entries.list(contentType),
     );
-    if (data.value) {
+    if (!error.value) {
       items.value = data.value.items;
     }
-  });
 }
 
 useHead({
   link: [{ rel: 'icon', type: 'image/png', href: 'favicon.ico' }]
-}) 
+})
 </script>
 
 <template>
